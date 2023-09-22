39d666021e140e9c6737267229af4c133ec331ee
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-22 01:46:19 -0500

    create header and pop up (modal)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header";
+import Advertisment from "./components/advertisment";
 
 function App() {
   return (
     <>
       <Header />
+      <Advertisment />
     </>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./header.module.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className={styles.headerBox}>
+        <h1>i m header </h1>
+      </div>
+    </>
+  );
 }
-
 export default Header;
